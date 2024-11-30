9f7d356e6c2c1ea9967d9381c4e2754ead5967b2
Erick559 <erickungu82@gmail.com>
2024-11-30 15:12:56 +0300

    Fix base and also remove resolve.

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  base:'/data-janitors/',
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
+  base:'/data-janitors-2/',
 })
